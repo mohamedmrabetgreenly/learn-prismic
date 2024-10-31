dabd6ae7912a3bd3064df4a57235f82a9b8bf139
Mohamed M'rabet <mohamed.mrabet@greenly.earth>
2024-10-31 18:59:32 +0100

    feat: Create Settings Custom Type

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -7,7 +7,7 @@ import config from "../slicemachine.config.json";
  */
 export const repositoryName =
   process.env.NEXT_PUBLIC_PRISMIC_ENVIRONMENT || config.repositoryName;
-
+ 
 /**
  * A list of Route Resolver objects that define how a document's `url` field is resolved.
  *
