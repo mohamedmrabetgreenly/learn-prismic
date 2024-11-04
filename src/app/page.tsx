f21c73acc2ef05b3af947abc5b565b49cabdfdc9
Mohamed M'rabet <mohamed.mrabet@greenly.earth>
2024-11-04 18:23:24 +0100

    feat: add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
-  return (
-    <div className="">
-      <h1 className="font-display text-5xl">Hello world !</h1>
-    </div>
-  );
+import { Metadata } from "next";
+import { createClient } from "@/prismicio";
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return <div className="text-sky-400">Homepage</div>;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage");
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+  };
 }
